9b4125d09a269863f9947ea2ec2bd6e357d5f7f9
Swaathi <swaathi1409@gmail.com>
2025-07-13 23:37:57 +0530

    update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(session({
 
 // MySQL Connection
 require('dotenv').config();
-const mysql = require('mysql2');
+
 const db = mysql.createConnection({
     host: process.env.DB_HOST || 'yamanote.proxy.rlwy.net',
     user: process.env.DB_USER || 'root', 
