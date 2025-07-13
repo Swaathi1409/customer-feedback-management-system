5f2c75f1311240337701b324665d1b699d5fe54e
Swaathi <swaathi1409@gmail.com>
2025-07-13 23:33:19 +0530

    changed local mysql to cloud mysql for deployment

--- app.js
+++ app.js
@@ -33,13 +33,13 @@ app.use(session({
 
 // MySQL Connection
 require('dotenv').config();
-
+const mysql = require('mysql2');
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'yamanote.proxy.rlwy.net',
     user: process.env.DB_USER || 'root', 
     password: process.env.DB_PASSWORD || '', 
-    database: process.env.DB_NAME || 'cfms',
-    port: process.env.DB_PORT || 3306
+    database: process.env.DB_NAME || 'railway',
+    port: process.env.DB_PORT || 15558
 });
 
 // Connect to MySQL
